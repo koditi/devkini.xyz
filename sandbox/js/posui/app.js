de5c7e6ffd1fc377e71f450aa0b37aaf6c45c1a4
crynobone <crynobone@gmail.com>
2016-01-01 14:28:45 +0800

    Add search filter.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- sandbox/js/posui/app.js
+++ sandbox/js/posui/app.js
@@ -16,8 +16,11 @@ new Vue({
             { name: 'Item-5', baracode: _.uniqueId('bar'), price: 15},
             { name: 'Item-6', baracode: _.uniqueId('bar'), price: 5},
             { name: 'Item-7', baracode: _.uniqueId('bar'), price: 11},
-            { name: 'Item-8', baracode: _.uniqueId('bar'), price: 10}
-        ]
+            { name: 'Item-8', baracode: _.uniqueId('bar'), price: 10},
+            { name: 'Item-9', baracode: _.uniqueId('bar'), price: 10},
+            { name: 'Item-10', baracode: _.uniqueId('bar'), price: 10}
+        ],
+        search: ''
     },
     methods: {
         addOrderItem: function(event) {
