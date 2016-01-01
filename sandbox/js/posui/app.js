ef265d47104d06edab910cf6a2626844f9976934
crynobone <crynobone@gmail.com>
2016-01-01 14:25:46 +0800

    Simplify code and add debugging mode.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- sandbox/js/posui/app.js
+++ sandbox/js/posui/app.js
@@ -9,18 +9,14 @@ new Vue({
             }
         },
         items: [
-            [
-                { name: 'Item-1', price: 15},
-                { name: 'Item-2', price: 5},
-                { name: 'Item-3', price: 11},
-                { name: 'Item-4', price: 10}
-            ],
-            [
-                { name: 'Item-5', price: 15},
-                { name: 'Item-6', price: 5},
-                { name: 'Item-7', price: 11},
-                { name: 'Item-8', price: 10}
-            ]
+            { name: 'Item-1', baracode: _.uniqueId('bar'), price: 15},
+            { name: 'Item-2', baracode: _.uniqueId('bar'), price: 5},
+            { name: 'Item-3', baracode: _.uniqueId('bar'), price: 11},
+            { name: 'Item-4', baracode: _.uniqueId('bar'), price: 10},
+            { name: 'Item-5', baracode: _.uniqueId('bar'), price: 15},
+            { name: 'Item-6', baracode: _.uniqueId('bar'), price: 5},
+            { name: 'Item-7', baracode: _.uniqueId('bar'), price: 11},
+            { name: 'Item-8', baracode: _.uniqueId('bar'), price: 10}
         ]
     },
     methods: {
