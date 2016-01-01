d769abc73d331abec918a6250a90ade9ab9cdb77
crynobone <crynobone@gmail.com>
2016-01-01 14:46:58 +0800

    Convert order/checkout to a vue component.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- sandbox/js/posui/app.js
+++ sandbox/js/posui/app.js
@@ -1,6 +1,17 @@
+Vue.component('checkout', {
+    template: '#checkout-template',
+    props: ['orders'],
+    methods: {
+        removeOrder: function(item) {
+            this.orders.$remove(item);
+        }
+    }
+});
+
 new Vue({
     el: '#app',
     data: {
+        orders: [],
         order: {
             items: [
             ],
@@ -23,24 +34,19 @@ new Vue({
         search: ''
     },
     methods: {
-        addOrderItem: function(event) {
-            console.log(event);
-            var item, item_name, item_price, item_qty = 1;
+        addOrder: function(item) {
+            var order, quantity = 1;
 
-            item_name = event.target.getAttribute('data-name');
-            item_price = event.target.getAttribute('data-price');
-            item = _.find(this.order.items, {name: item_name});
-            if (item) {
-                item_qty = item.qty++;
+            order = _.find(this.orders, {name: item.name});
+
+            if (order) {
+                quantity = order.qty++;
+            } else {
+                order = {name: item.name, price: item.price, qty: quantity, total: item.price};
+                this.orders.push(order);
             }
-            else {
-                item = {name: item_name, price: item_price, qty: item_qty, total: item_price};
-                this.order.items.push(item);
-            }
-            item.total = item.price * item.qty;
-        },
-        removeItem: function(event, item) {
-            this.order.items.$remove(item);
+
+            order.total = order.price * order.qty;
         }
     }
 });
